59ac6308adbec681b2105a35dc35075c6de67624
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-11-24 18:10:52 +0900

    Add waveform transforms

---
+++ transforms/waveform.py
@@ -0,0 +1,172 @@
+import colorednoise as cn
+import librosa
+import numpy as np
+
+
+def get_waveform_transforms(config: dict, phase: str):
+    transforms = config.get("transforms")
+    if transforms is None:
+        return None
+    else:
+        if transforms[phase] is None:
+            return None
+        trns_list = []
+        for trns_conf in transforms[phase]:
+            trns_name = trns_conf["name"]
+            trns_params = {} if trns_conf.get("params") is None else trns_conf["params"]
+            if globals().get(trns_name) is not None:
+                trns_cls = globals()[trns_name]
+                trns_list.append(trns_cls(**trns_params))
+
+        if len(trns_list) > 0:
+            return Compose(trns_list)
+        else:
+            return None
+
+
+class Compose:
+    def __init__(self, transforms: list):
+        self.transforms = transforms
+
+    def __call__(self, y: np.ndarray):
+        for trns in self.transforms:
+            y = trns(y)
+        return y
+
+
+class OneOf:
+    def __init__(self, transforms: list):
+        self.transforms = transforms
+
+    def __call__(self, y: np.ndarray):
+        n_trns = len(self.transforms)
+        trns_idx = np.random.choice(n_trns)
+        trns = self.transforms[trns_idx]
+        y = trns(y)
+        return y
+
+
+class AudioTransform:
+    def __init__(self, always_apply=False, p=0.5):
+        self.always_apply = always_apply
+        self.p = p
+
+    def __call__(self, y: np.ndarray):
+        if self.always_apply:
+            return self.apply(y)
+        else:
+            if np.random.rand() < self.p:
+                return self.apply(y)
+            else:
+                return y
+
+    def apply(self, y: np.ndarray):
+        raise NotImplementedError
+
+
+class GaussianNoiseSNR(AudioTransform):
+    def __init__(self, always_apply=False, p=0.5, min_snr=5.0, max_snr=20.0, **kwargs):
+        super().__init__(always_apply, p)
+
+        self.min_snr = min_snr
+        self.max_snr = max_snr
+
+    def apply(self, y: np.ndarray, **params):
+        snr = np.random.uniform(self.min_snr, self.max_snr)
+        a_signal = np.sqrt(y ** 2).max()
+        a_noise = a_signal / (10 ** (snr / 20))
+
+        white_noise = np.random.randn(len(y))
+        a_white = np.sqrt(white_noise ** 2).max()
+        augmented = (y + white_noise * 1 / a_white * a_noise).astype(y.dtype)
+        return augmented
+
+
+class PinkNoiseSNR(AudioTransform):
+    def __init__(self, always_apply=False, p=0.5, min_snr=5.0, max_snr=20.0, **kwargs):
+        super().__init__(always_apply, p)
+
+        self.min_snr = min_snr
+        self.max_snr = max_snr
+
+    def apply(self, y: np.ndarray, **params):
+        snr = np.random.uniform(self.min_snr, self.max_snr)
+        a_signal = np.sqrt(y ** 2).max()
+        a_noise = a_signal / (10 ** (snr / 20))
+
+        pink_noise = cn.powerlaw_psd_gaussian(1, len(y))
+        a_pink = np.sqrt(pink_noise ** 2).max()
+        augmented = (y + pink_noise * 1 / a_pink * a_noise).astype(y.dtype)
+        return augmented
+
+
+class PitchShift(AudioTransform):
+    def __init__(self, always_apply=False, p=0.5, max_steps=5, sr=32000):
+        super().__init__(always_apply, p)
+
+        self.max_steps = max_steps
+        self.sr = sr
+
+    def apply(self, y: np.ndarray, **params):
+        n_steps = np.random.randint(-self.max_steps, self.max_steps)
+        augmented = librosa.effects.pitch_shift(y, sr=self.sr, n_steps=n_steps)
+        return augmented
+
+
+class TimeStretch(AudioTransform):
+    def __init__(self, always_apply=False, p=0.5, max_rate=1.2):
+        super().__init__(always_apply, p)
+
+        self.max_rate = max_rate
+
+    def apply(self, y: np.ndarray, **params):
+        rate = np.random.uniform(0, self.max_rate)
+        augmented = librosa.effects.time_stretch(y, rate)
+        return augmented
+
+
+class TimeShift(AudioTransform):
+    def __init__(self, always_apply=False, p=0.5, max_shift_second=2, sr=32000, padding_mode="replace"):
+        super().__init__(always_apply, p)
+
+        assert padding_mode in ["replace", "zero"], "`padding_mode` must be either 'replace' or 'zero'"
+        self.max_shift_second = max_shift_second
+        self.sr = sr
+        self.padding_mode = padding_mode
+
+    def apply(self, y: np.ndarray, **params):
+        shift = np.random.randint(-self.sr * self.max_shift_second, self.sr * self.max_shift_second)
+        augmented = np.roll(y, shift)
+        if self.padding_mode == "zero":
+            if shift > 0:
+                augmented[:shift] = 0
+            else:
+                augmented[shift:] = 0
+        return augmented
+
+
+class VolumeControl(AudioTransform):
+    def __init__(self, always_apply=False, p=0.5, db_limit=10, mode="uniform"):
+        super().__init__(always_apply, p)
+
+        assert mode in ["uniform", "fade", "fade", "cosine", "sine"], \
+            "`mode` must be one of 'uniform', 'fade', 'cosine', 'sine'"
+
+        self.db_limit = db_limit
+        self.mode = mode
+
+    def apply(self, y: np.ndarray, **params):
+        db = np.random.uniform(-self.db_limit, self.db_limit)
+        if self.mode == "uniform":
+            db_translated = 10 ** (db / 20)
+        elif self.mode == "fade":
+            lin = np.arange(len(y))[::-1] / (len(y) - 1)
+            db_translated = 10 ** (db * lin / 20)
+        elif self.mode == "cosine":
+            cosine = np.cos(np.arange(len(y)) / len(y) * np.pi * 2)
+            db_translated = 10 ** (db * cosine / 20)
+        else:
+            sine = np.sin(np.arange(len(y)) / len(y) * np.pi * 2)
+            db_translated = 10 ** (db * sine / 20)
+        augmented = y * db_translated
+        return augmented
