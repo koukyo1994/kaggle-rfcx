ae480f5093bb3e30b8a377dcdc519fb19bb7e41c
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-11-30 13:13:43 +0900

    Use joblib

--- input/resample.py
+++ input/resample.py
@@ -4,15 +4,17 @@ import librosa
 
 import soundfile as sf
 
+from joblib import delayed, Parallel
 from pathlib import Path
-from tqdm import tqdm
 
 
-def resample(audio_path: Path, save_path: Path, target_sr=32000):
+def resample(audio_path: Path, save_dir: Path, target_sr=32000):
     y, _ = librosa.load(audio_path,
                         sr=target_sr,
                         mono=True,
                         res_type="kaiser_best")
+    audio_name = audio_path.name
+    save_path = save_dir / audio_name.replace(".flac", ".wav")
     sf.write(save_path, y, samplerate=target_sr)
 
 
@@ -29,17 +31,13 @@ if __name__ == "__main__":
     save_dir.mkdir(exist_ok=True, parents=True)
 
     audio_paths = list(audio_dir.glob("*.flac"))
-    for audio_path in tqdm(audio_paths, desc="train"):
-        audio_name = audio_path.name
-        save_path = save_dir / audio_name.replace(".flac", ".wav")
-        resample(audio_path, save_path, target_sr)
+    Parallel(n_jobs=20, verbose=10)(delayed(resample)(
+        audio_path, save_dir, target_sr) for audio_path in audio_paths)
 
     audio_dir = Path("./test")
     save_dir = Path(f"./test_{int(target_sr)}k")
     save_dir.mkdir(parents=True, exist_ok=True)
 
-    audio_path = list(audio_dir.glob("*.flac"))
-    for audio_path in tqdm(audio_paths, desc="test"):
-        audio_name = audio_path.name
-        save_path = save_dir / audio_name.replace(".flac", ".wav")
-        resample(audio_path, save_path, target_sr)
+    audio_paths = list(audio_dir.glob("*.flac"))
+    Parallel(n_jobs=20, verbose=10)(delayed(resample)(
+        audio_path, save_dir, target_sr) for audio_path in audio_paths)
