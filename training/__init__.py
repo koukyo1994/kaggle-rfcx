d7b746cd8af68026beecebb44abfe6c3aaf6bc38
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-12-03 23:16:35 +0900

    Add AdaBelief

--- training/__init__.py
+++ training/__init__.py
@@ -5,6 +5,13 @@ import torch.optim as optim
 from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 from sklearn import model_selection
 
+from .optimizers import AdaBelief
+
+
+__OPTIMIZERS__ = {
+    "AdaBelief": AdaBelief
+}
+
 
 def get_device(device: str):
     if torch.cuda.is_available() and "cuda" in device:
@@ -16,9 +23,12 @@ def get_device(device: str):
 def get_optimizer(model: nn.Module, config: dict):
     optimizer_config = config["optimizer"]
     optimizer_name = optimizer_config.get("name")
-
-    return optim.__getattribute__(optimizer_name)(model.parameters(),
-                                                  **optimizer_config["params"])
+    if __OPTIMIZERS__.get(optimizer_name) is not None:
+        return __OPTIMIZERS__[optimizer_name](model.parameters(),
+                                              **optimizer_config["params"])
+    else:
+        return optim.__getattribute__(optimizer_name)(model.parameters(),
+                                                      **optimizer_config["params"])
 
 
 def get_scheduler(optimizer, config: dict):
